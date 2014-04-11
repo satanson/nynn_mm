68f922733da038574525d303f8ab239822992a0b
satanson <ranpanf@gmail.com>
2014-04-11 10:53:55 +0800

    by satanson

--- src/cli/src/cli_framework.cpp
+++ src/cli/src/cli_framework.cpp
@@ -20,22 +20,34 @@ int main(int argc,char**argv)
 	log_i("connected to %s",serv_endpoint.c_str());
 	prot::Requester req(sock);
 
-	struct timeval beg_tv,end_tv;
+	struct timespec begin_ts,end_ts;
 	double t;
 	
 	cout<<"loop="<<loop<<endl;
-	long long int nbytes=0;
+
+	uint64_t concurrency=0;
+	uint64_t nbytes=0;
+	double  tbegin=0;
+	double  tend=0;
+	clock_gettime(CLOCK_MONOTONIC,&begin_ts);
 	
-	gettimeofday(&beg_tv,NULL);
 	for (int i=0;i<loop;i++){
 		req.ask(0,NULL,0,NULL,0);
 		req.parse_ans();
+		concurrency++;
 		nbytes+=req.get_data_size();
 	}
-	gettimeofday(&end_tv,NULL);
-	t=((end_tv.tv_sec*1000+end_tv.tv_usec/1000)-(beg_tv.tv_sec*1000+beg_tv.tv_usec/1000))/1000.0;
-	cout<<"time elapse:"<<t<<"s"<<endl;
-	cout<<"nbytes:"<<nbytes<<"B"<<endl;
-	cout<<"concurrency:"<<loop/t<<endl;
-	cout<<"throughput:"<<nbytes/t/(1024.0*1024.0)<<" MB/s"<<endl;
+	clock_gettime(CLOCK_MONOTONIC,&end_ts);
+	tbegin=begin_ts.tv_sec+begin_ts.tv_nsec/1.0e9;
+	tend=end_ts.tv_sec+end_ts.tv_nsec/1.0e9;
+	t=tend-tbegin;
+	cout.setf(ios::fixed);
+	cout<<"tbegin:"<<tbegin<<endl;
+	cout<<"tend:"<<tend<<endl;
+	cout<<"t:"<<t<<endl;
+	cout<<"nbytes:"<<nbytes<<endl;
+	cout<<"concurrency:"<<concurrency<<endl;
+	cout<<"throughput:"<<nbytes/1024.0/1024.0<<endl;
+	cout<<"ave_concurrency:"<<concurrency/t<<endl;
+	cout<<"ave_throughput:"<<nbytes/t/1024.0/1024.0<<endl;
 }
