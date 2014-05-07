b4ed43819908949a6fc2bdf0fe640258bb1b6629
satanson <ranpanf@gmail.com>
2014-05-07 10:29:20 +0800

    by satanson

--- src/cli/src/cli_clearvtx.cpp
+++ src/cli/src/cli_clearvtx.cpp
@@ -27,6 +27,6 @@ int main(int argc,char**argv){
 	log_i("connected to %s",serv_endpoint.c_str());
 	prot::Requester req(sock);
 	
-	while((*act)(req,vtxno,0,&blk)!=INVALID_BLOCKNO);
+	while((*act)(req,vtxno,0,NULL)!=INVALID_BLOCKNO);
 	sock.close();
 }
