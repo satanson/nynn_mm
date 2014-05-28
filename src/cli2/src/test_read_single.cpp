3114d48a84ceca5d9a7ef3bc809b80ebe355863e
satanson <ranpanf@gmail.com>
2014-05-29 00:58:59 +0800

    05/29/14:00:58:59

--- src/cli2/src/test_read_single.cpp
+++ src/cli2/src/test_read_single.cpp
@@ -17,6 +17,7 @@ long getTime()
 int main(int argc,char**argv){
     uint32_t vtxno=atoi(argv[1]);
     int vtxNum=atoi(argv[2]);
+    int reply=atoi(argv[3]);
     uint32_t vtxend=vtxno+vtxNum;
     double counts=0;
 	nynn_fs fs("192.168.255.114:50000","192.168.255.114:60000");
@@ -24,16 +25,20 @@ int main(int argc,char**argv){
 	CharContent *cctt=blk;
     int i=0,j;
     long time_pre=getTime();
-    while(vtxno<vtxend){
-   	    uint32_t blkno=nynn_file::headblkno;
-        nynn_file f(fs,vtxno,false);
-        while(blkno!=nynn_file::invalidblkno){
-			if(!f.read(blkno,&blk)) break;
-            blkno=(blk.getHeader()->getNext)();
-            string line(cctt->begin(),cctt->end());
-            counts+=line.size();
-        }
-        vtxno++;
+    while(reply>0){
+    	vtxno=atoi(argv[1]);
+    	while(vtxno<vtxend){
+   	    	uint32_t blkno=nynn_file::headblkno;
+        	nynn_file f(fs,vtxno,false);
+        	while(blkno!=nynn_file::invalidblkno){
+				if(!f.read(blkno,&blk)) break;
+            	blkno=(blk.getHeader()->getNext)();
+            	string line(cctt->begin(),cctt->end());
+            	counts+=line.size();
+        	}
+        	vtxno++;
+   	   }
+   	   reply--;
    }
    long time_next=getTime();
    cout<<"time:"<<time_next-time_pre<<"ms datasize:"<<counts<<"B throughout:"<<counts/1024/1024/(time_next-time_pre)*1000<<"MB/s"; 
