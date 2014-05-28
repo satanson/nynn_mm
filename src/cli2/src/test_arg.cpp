60f8990fed0ec45521fc5aa33b7193ac2cccadbc
satanson <ranpanf@gmail.com>
2014-05-28 18:43:28 +0800

    05/28/14:18:43:27

---
+++ src/cli2/src/test_arg.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(int argc,char** argv)
+{
+	int i=atoi(argv[1]);
+    cout<<argc<<" "<<i;
+    return i;
+}
--- src/cli2/src/test_write_single.cpp
+++ src/cli2/src/test_write_single.cpp
@@ -1,44 +1,49 @@
 #include<nynn_fs.hpp>
 #include<nynn_file.hpp>
 #include<sys/time.h>
+#include<stdlib.h>
 using namespace std;
 using namespace nynn;
 using namespace nynn::mm;
 using namespace nynn::cli;
 
-//retun current time "ms"
 long getTime()
 {
 	struct timeval tv;
     gettimeofday(&tv,NULL);
     return tv.tv_sec*1000+tv.tv_usec/1000;
 }
+
 int main(int argc,char**argv){
-  
     string data;
     data.resize(CharContent::CONTENT_CAPACITY);
-    uint32_t vtxno;
+    double throughout=0; 
+    uint32_t vtxno=atoi(argv[1]);
+    int vtxNum=atoi(argv[2]);
+    int blkNum=atoi(argv[3]);
+    int vtxend=vtxno+vtxNum;
+    int reply=atoi(argv[4]);
 	nynn_fs fs("192.168.255.114:50000","192.168.255.114:60000");
 	Block blk;
 	CharContent *cctt=blk;
     int i=0,j;
     long time_pre=getTime();
-	while(i<16){
-        vtxno=0;
-        while(vtxno<1024){
+	while(i<reply){
+        vtxno=atoi(argv[1]);
+        while(vtxno<vtxend){
             j=0;
         	nynn_file f(fs,vtxno,true);
-            while(j<4){
+            while(j<blkNum){
 				cctt->resize(data.size());
 				std::copy(data.begin(),data.end(),cctt->begin());
 				f.push(&blk);
+                throughout+=data.size();
                 j++;
             }
-           // cout<<vtxno<<"as"<<i<<" is over"<<endl;
         	vtxno++;
-        }
+       }
        i++;
    }
    long time_next=getTime();
-   cout<<time_next-time_pre; 
+   cout<<"time:"<<time_next-time_pre<<" datasize:"<<throughout<<"B throurhout:"<<throughout/1024/1024/(time_next-time_pre)/1000<<"MB/s"; 
 }
